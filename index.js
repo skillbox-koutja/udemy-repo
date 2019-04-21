c06da1db78bbab751f1f21dac34b4345a83b5fdb
Kutyakin Micha <skillbox-koutja@yandex.ru>
2019-04-21 22:46:09 +0300

    add loops and conditions

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-let money = prompt('Ваш бюджет на месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
+let money = prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
     budget: +money,
@@ -15,8 +15,28 @@ let appData = {
 
 let questions = 2;
 while (questions--) {
-    let expense = prompt('Введите обязательную статью расходов в этом месяце');
-    let cost = prompt('Во сколько обойдется?');
-    appData.expenses[expense] = +cost;
+    let expense = prompt('Введите обязательную статью расходов в этом месяце', '');
+    let cost = prompt('Во сколько обойдется?', '');
+    if (typeof (expense) === 'string' && typeof (cost) === 'string' &&
+        expense !== '' && cost !== '' &&
+        expense.length < 50
+    ) {
+        appData.expenses[expense] = +cost;
+    } else {
+        questions++;
+    }
 }
-alert(appData.budget / 30);
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert(appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay >= 2000) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Произошла ошибка');
+}
