efc233981ddfcd9aa0ac2b72d6a4cf1490a4a0a0
Kutyakin Micha <kutyakinma@modeltech.ru>
2019-04-21 21:43:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+let money = prompt('Ваш бюджет на месяц?');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+
+let appData = {
+    budget: +money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+};
+
+let questions = 2;
+while (questions--) {
+    let expense = prompt('Введите обязательную статью расходов в этом месяце');
+    let cost = prompt('Во сколько обойдется?');
+    appData[expense] = +cost;
+}
+alert(appData.budget / 30);
